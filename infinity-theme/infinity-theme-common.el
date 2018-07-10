a38e435099d478aa5c10eebf4d330047d5d338f9
Tommy X <tommyx058@gmail.com>
2018-07-10 01:44:17 -0400

    .

--- infinity-theme/infinity-theme-common.el
+++ infinity-theme/infinity-theme-common.el
@@ -164,7 +164,7 @@
     `(font-lock-type-face ((,class (:foreground ,type))))
     `(font-lock-variable-name-face ((,class (:foreground ,var))))
     `(font-lock-warning-face ((,class (:foreground ,war :background ,bg1))))
-    `(fringe ((,class (:background ,bg1 :foreground ,base))))
+    `(fringe ((,class (:background ,bg1 :foreground ,lnum))))
     `(header-line ((,class :background ,bg4 :inherit (mode-line))))
     `(highlight ((,class (:foreground ,base :background ,highlight))))
     `(hl-line ((,class (:background ,bg2))))
