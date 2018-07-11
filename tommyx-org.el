452046eb86547a15648c88a87a531619ace648b3
Tommy X <tommyx058@gmail.com>
2018-07-11 02:31:29 -0400

    .

--- tommyx-org.el
+++ tommyx-org.el
@@ -29,25 +29,27 @@
 
 ;; super agenda groups
 (setq org-super-agenda-groups '(
-    (:name "Time Grid"
-        :time-grid t
-    )
-    (:name "Due"
-        :deadline today
-        :deadline past
-    )
-    (:name "Important + Urgent"
-        :and (:priority "A")
-    )
-    (:name "Primary"
-        :and (:priority "B")
-    )
-    (:name "Recurring"
-        :and (:tag ("recurring"))
-    )
-    (:name "Secondary"
-        :and (:priority "C")
-    )
+	(:name "Time Grid"
+		:time-grid t
+	)
+	(:name "Overdue"
+		:deadline past
+	)
+	(:name "Due"
+		:deadline today
+	)
+	(:name "Important + Urgent"
+		:and (:priority "A")
+	)
+	(:name "Primary"
+		:and (:priority "B")
+	)
+	(:name "Recurring"
+		:and (:tag ("recurring"))
+	)
+	(:name "Secondary"
+		:and (:priority "C")
+	)
 	(:name "Other Items"
 		:anything
 	)
@@ -56,13 +58,13 @@
 ;; clocking
 ; ask for clock-out on leave
 (defun my/org-clock-query-out ()
-    "Ask the user before clocking out.
-    This is a useful function for adding to `kill-emacs-query-functions'."
-    (if (and
-        (featurep 'org-clock)
-        (funcall 'org-clocking-p)
-        (y-or-n-p "You are currently clocking time, clock out? "))
-        (org-clock-out) t)) ;; only fails on keyboard quit or error
+	"Ask the user before clocking out.
+	This is a useful function for adding to `kill-emacs-query-functions'."
+	(if (and
+		(featurep 'org-clock)
+		(funcall 'org-clocking-p)
+		(y-or-n-p "You are currently clocking time, clock out? "))
+		(org-clock-out) t)) ;; only fails on keyboard quit or error
 (add-hook 'kill-emacs-query-functions 'my/org-clock-query-out) ; timeclock.el puts this on the wrong hook!
 
 ;; agenda configs
@@ -99,15 +101,15 @@
 
 ;; capture templates
 (setq org-capture-templates '(
-    ("I" "Important" entry
-    (file+headline "GTD.org" "Important")
-    "* %? %i")
-    ("i" "Not important" entry
-    (file+headline "GTD.org" "Not important")
-    "* %? %i")
-    ("m" "Misc TODO" entry
-    (file+headline "GTD.org" "Misc")
-    "* TODO %? %i")
+	("I" "Important" entry
+	(file+headline "GTD.org" "Important")
+	"* %? %i")
+	("i" "Not important" entry
+	(file+headline "GTD.org" "Not important")
+	"* %? %i")
+	("m" "Misc TODO" entry
+	(file+headline "GTD.org" "Misc")
+	"* TODO %? %i")
 ))
 
 ;; checks if org-notes-dir exists.
@@ -116,62 +118,62 @@
 ;; key bindings
 ; global leader
 (general-define-key
-    :keymaps 'override
-    :states '(motion normal visual)
-    :prefix "SPC"
+	:keymaps 'override
+	:states '(motion normal visual)
+	:prefix "SPC"
 
 )
 (general-define-key
-    :keymaps 'override
-    :states '(motion normal)
-    :prefix "SPC"
+	:keymaps 'override
+	:states '(motion normal)
+	:prefix "SPC"
 
-    "of" '(counsel-org-goto-all ; go to heading of opened org files
-        :which-key "org goto all")
+	"of" '(counsel-org-goto-all ; go to heading of opened org files
+		:which-key "org goto all")
 
-    "oc" '(org-capture
-        :which-key "org capture")
+	"oc" '(org-capture
+		:which-key "org capture")
 
-    "oa" '(org-agenda
-        :which-key "org agenda")
+	"oa" '(org-agenda
+		:which-key "org agenda")
 
-    "oi" '(org-pomodoro
-        :which-key "org pomo clock in")
+	"oi" '(org-pomodoro
+		:which-key "org pomo clock in")
 )
 ; org mode leader
 (general-define-key
-    :keymaps 'org-mode-map
-    :states '(motion normal visual)
-    :prefix "SPC"
+	:keymaps 'org-mode-map
+	:states '(motion normal visual)
+	:prefix "SPC"
 
-    "jf" '(counsel-org-goto
-        :which-key "org goto")
+	"jf" '(counsel-org-goto
+		:which-key "org goto")
 )
 (general-define-key
-    :keymaps 'org-mode-map
-    :states '(motion normal)
-    :prefix "SPC"
+	:keymaps 'org-mode-map
+	:states '(motion normal)
+	:prefix "SPC"
 
-    "jr" '(org-refile
-        :which-key "org refile")
+	"jr" '(org-refile
+		:which-key "org refile")
 
-    "jt" '(org-todo
-        :which-key "org todo")
+	"jt" '(org-todo
+		:which-key "org todo")
 
-    "js" '(org-schedule
-        :which-key "org schedule")
+	"js" '(org-schedule
+		:which-key "org schedule")
 
-    "jd" '(org-deadline
-        :which-key "org deadline")
+	"jd" '(org-deadline
+		:which-key "org deadline")
 
-    "jp" '(org-priority
-        :which-key "org priority")
+	"jp" '(org-priority
+		:which-key "org priority")
 
-    "jq" '(org-set-tags-command
-        :which-key "org set tags")
+	"jq" '(org-set-tags-command
+		:which-key "org set tags")
 
-    "ji" '(org-pomodoro
-        :which-key "org pomo clock in")
+	"ji" '(org-pomodoro
+		:which-key "org pomo clock in")
 )
 ; org mode
 (evil-define-key 'insert org-mode-map (kbd "TAB") 'org-cycle)
@@ -208,37 +210,37 @@
   ;; C-c c: initiate org capture. can be used everywhere.
 
   ;; remove bindings
-    (kbd "M-j") nil
-    (kbd "M-k") nil
+	(kbd "M-j") nil
+	(kbd "M-k") nil
 
-    (kbd "C-c C-.") 'org-time-stamp-inactive ; with C-u as previx also add time.
+	(kbd "C-c C-.") 'org-time-stamp-inactive ; with C-u as previx also add time.
 
-    (kbd "C--") 'org-shiftdown ; change date like speed-dating
-    (kbd "C-=") 'org-shiftup
+	(kbd "C--") 'org-shiftdown ; change date like speed-dating
+	(kbd "C-=") 'org-shiftup
 
-    (kbd "C-S-h") 'org-shiftmetaleft ; promote/outdent
-    (kbd "C-S-j") 'org-metadown ; move down
-    (kbd "C-S-k") 'org-metaup ; move up
-    (kbd "C-S-l") 'org-shiftmetaright ; demote/indent
-    (kbd "M-h") help-map
+	(kbd "C-S-h") 'org-shiftmetaleft ; promote/outdent
+	(kbd "C-S-j") 'org-metadown ; move down
+	(kbd "C-S-k") 'org-metaup ; move up
+	(kbd "C-S-l") 'org-shiftmetaright ; demote/indent
+	(kbd "M-h") help-map
 
-    (kbd "M-RET") 'org-insert-heading-respect-content
-    (kbd "M-S-RET") 'org-insert-todo-heading-respect-content
-    (kbd "C-RET") 'org-insert-subheading
-    (kbd "C-S-RET") 'org-insert-todo-subheading
-    (kbd "<M-return>") 'org-insert-heading-respect-content
-    (kbd "<M-S-return>") 'org-insert-todo-heading-respect-content
-    (kbd "<C-return>") 'org-insert-subheading
-    (kbd "<C-S-return>") 'org-insert-todo-subheading
+	(kbd "M-RET") 'org-insert-heading-respect-content
+	(kbd "M-S-RET") 'org-insert-todo-heading-respect-content
+	(kbd "C-RET") 'org-insert-subheading
+	(kbd "C-S-RET") 'org-insert-todo-subheading
+	(kbd "<M-return>") 'org-insert-heading-respect-content
+	(kbd "<M-S-return>") 'org-insert-todo-heading-respect-content
+	(kbd "<C-return>") 'org-insert-subheading
+	(kbd "<C-S-return>") 'org-insert-todo-subheading
 
-    (kbd "C-h") (lambda () (interactive) (outline-hide-subtree))
-    (kbd "C-j") 'org-next-visible-heading
-    (kbd "C-k") 'org-previous-visible-heading
-    ;; (kbd "C-l") (lambda () (interactive) (outline-show-entry) (outline-show-children))
-    (kbd "C-l") 'org-cycle
+	(kbd "C-h") (lambda () (interactive) (outline-hide-subtree))
+	(kbd "C-j") 'org-next-visible-heading
+	(kbd "C-k") 'org-previous-visible-heading
+	;; (kbd "C-l") (lambda () (interactive) (outline-show-entry) (outline-show-children))
+	(kbd "C-l") 'org-cycle
 
-    "X" 'outline-show-all
-    "Z" 'org-shifttab ; cycle global visibility
+	"X" 'outline-show-all
+	"Z" 'org-shifttab ; cycle global visibility
 )
 (evil-define-key 'insert org-mode-map
   ;; using evil-collection with org mode:
@@ -251,64 +253,64 @@
   ;; TAB and S-TAB: go through table fields
   ;; RET: table next row
 
-    (kbd "C-c C-.") 'org-time-stamp-inactive ; with C-u as previx also add time.
+	(kbd "C-c C-.") 'org-time-stamp-inactive ; with C-u as previx also add time.
 
-    (kbd "M-RET") 'org-insert-heading-respect-content
-    (kbd "M-S-RET") 'org-insert-todo-heading-respect-content
-    (kbd "C-RET") 'org-insert-subheading
-    (kbd "C-S-RET") 'org-insert-todo-subheading
-    (kbd "<M-return>") 'org-insert-heading-respect-content
-    (kbd "<M-S-return>") 'org-insert-todo-heading-respect-content
-    (kbd "<C-return>") 'org-insert-subheading
-    (kbd "<C-S-return>") 'org-insert-todo-subheading
+	(kbd "M-RET") 'org-insert-heading-respect-content
+	(kbd "M-S-RET") 'org-insert-todo-heading-respect-content
+	(kbd "C-RET") 'org-insert-subheading
+	(kbd "C-S-RET") 'org-insert-todo-subheading
+	(kbd "<M-return>") 'org-insert-heading-respect-content
+	(kbd "<M-S-return>") 'org-insert-todo-heading-respect-content
+	(kbd "<C-return>") 'org-insert-subheading
+	(kbd "<C-S-return>") 'org-insert-todo-subheading
 
-    (kbd "M-h") help-map
+	(kbd "M-h") help-map
 )
 (evil-define-motion evil-org-next-visible-heading () :type exclusive
-    (org-next-visible-heading 1))
+	(org-next-visible-heading 1))
 (evil-define-motion evil-org-previous-visible-heading () :type exclusive
-    (org-previous-visible-heading 1))
+	(org-previous-visible-heading 1))
 (evil-define-key 'visual org-mode-map
-    (kbd "C-S-h") (lambda () (interactive) (org-metaleft) (evil-visual-restore)) ; promote/outdent
-    (kbd "C-S-l") (lambda () (interactive) (org-metaright) (evil-visual-restore)) ; demote/indent
-    (kbd "M-h") help-map
+	(kbd "C-S-h") (lambda () (interactive) (org-metaleft) (evil-visual-restore)) ; promote/outdent
+	(kbd "C-S-l") (lambda () (interactive) (org-metaright) (evil-visual-restore)) ; demote/indent
+	(kbd "M-h") help-map
 
-    (kbd "C-j") 'evil-org-next-visible-heading
-    (kbd "C-k") 'evil-org-previous-visible-heading
+	(kbd "C-j") 'evil-org-next-visible-heading
+	(kbd "C-k") 'evil-org-previous-visible-heading
 )
 (evil-define-key 'motion 'global ",,n" 'org-narrow-to-subtree)
 ; org agenda
 (general-define-key
-    :keymaps 'org-agenda-mode-map
-    :states '(motion normal visual)
-    :prefix "SPC"
+	:keymaps 'org-agenda-mode-map
+	:states '(motion normal visual)
+	:prefix "SPC"
 
 )
 (general-define-key
-    :keymaps 'org-agenda-mode-map
-    :states '(motion normal)
-    :prefix "SPC"
+	:keymaps 'org-agenda-mode-map
+	:states '(motion normal)
+	:prefix "SPC"
 
-    "jr" '(org-agenda-refile
-        :which-key "org agenda refile")
+	"jr" '(org-agenda-refile
+		:which-key "org agenda refile")
 
-    "jt" '(org-agenda-todo
-        :which-key "org agenda todo")
+	"jt" '(org-agenda-todo
+		:which-key "org agenda todo")
 
-    "js" '(org-agenda-schedule
-        :which-key "org agenda schedule")
+	"js" '(org-agenda-schedule
+		:which-key "org agenda schedule")
 
-    "jd" '(org-agenda-deadline
-        :which-key "org agenda deadline")
+	"jd" '(org-agenda-deadline
+		:which-key "org agenda deadline")
 
-    "jp" '(org-agenda-priority
-        :which-key "org agenda priority")
+	"jp" '(org-agenda-priority
+		:which-key "org agenda priority")
 
-    "jq" '(org-agenda-set-tags
-        :which-key "org agenda set tags")
+	"jq" '(org-agenda-set-tags
+		:which-key "org agenda set tags")
 
-    "ji" '(org-pomodoro
-        :which-key "org pomo clock in")
+	"ji" '(org-pomodoro
+		:which-key "org pomo clock in")
 )
 (evil-define-key 'motion org-agenda-mode-map
   ;; C-c C-t: make into todo / cycle todo states
@@ -323,26 +325,26 @@
   ;; r: refresh
   ;; q: quit
 
-    "Z" (lambda () (interactive) (when org-agenda-entry-text-mode (org-agenda-entry-text-mode)))
-    "X" (lambda () (interactive) (when (not org-agenda-entry-text-mode) (org-agenda-entry-text-mode)))
+	"Z" (lambda () (interactive) (when org-agenda-entry-text-mode (org-agenda-entry-text-mode)))
+	"X" (lambda () (interactive) (when (not org-agenda-entry-text-mode) (org-agenda-entry-text-mode)))
 
-    (kbd "M-h") help-map
+	(kbd "M-h") help-map
 
-    (kbd "C--") 'org-agenda-do-date-earlier
-    (kbd "C-=") 'org-agenda-do-date-later
+	(kbd "C--") 'org-agenda-do-date-earlier
+	(kbd "C-=") 'org-agenda-do-date-later
 
-    (kbd "C-h") 'org-agenda-earlier
-    (kbd "C-l") 'org-agenda-later
+	(kbd "C-h") 'org-agenda-earlier
+	(kbd "C-l") 'org-agenda-later
 
-    "r" 'org-agenda-redo
-    "u" (lambda () (interactive) (message "Temporarily disabled undo.")) ; 'org-agenda-undo
-    "U" 'org-agenda-redo
+	"r" 'org-agenda-redo
+	"u" (lambda () (interactive) (message "Temporarily disabled undo.")) ; 'org-agenda-undo
+	"U" 'org-agenda-redo
 
-    "j" 'org-agenda-next-line
-    "j" 'org-agenda-next-line
-    "k" 'org-agenda-previous-line
-    (kbd "C-j") 'org-agenda-next-date-line
-    (kbd "C-k") 'org-agenda-previous-date-line
+	"j" 'org-agenda-next-line
+	"j" 'org-agenda-next-line
+	"k" 'org-agenda-previous-line
+	(kbd "C-j") 'org-agenda-next-date-line
+	(kbd "C-k") 'org-agenda-previous-date-line
 
-    (kbd "C-c v") 'org-agenda-view-mode-dispatch
+	(kbd "C-c v") 'org-agenda-view-mode-dispatch
 )
